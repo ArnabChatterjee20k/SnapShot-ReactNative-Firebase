6cc152c23d3e17c8b7919ca79c5d445be8897acf
arnab <arnabchatterjee.ac.2@gmail.com>
2023-03-30 11:37:50 +0530

    Added google consent screen

--- components/SocialButton.jsx
+++ components/SocialButton.jsx
@@ -1,13 +1,23 @@
 import { View, Text } from "react-native";
 import React from "react";
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
 import {
   FacebookSocialButton,
   GoogleSocialButton,
 } from "react-native-social-buttons";
+
+WebBrowser.maybeCompleteAuthSession();
+
 export default function SocialButton() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "380957708851-f90bndntfngtcmp5r9t20kfcls9bmik9.apps.googleusercontent.com",
+    clientId:"380957708851-f90bndntfngtcmp5r9t20kfcls9bmik9.apps.googleusercontent.com"
+  });
   return (
     <View style={{ gap: 5 }}>
-      <GoogleSocialButton />
+      <GoogleSocialButton onPress={()=>promptAsync()}/>
       <FacebookSocialButton />
     </View>
   );
